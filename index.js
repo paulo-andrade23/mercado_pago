534a555e8bb3184f00acaa3f199da8c054dd89af
Paulo Andrade <119083591+paulo-andrade23@users.noreply.github.com>
2023-12-28 14:10:16 -0300

    Update index.js

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ const PORT = 443 || "http://localhost:443";
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Soy el server :)");
-});
+app.get("/index", (req, res) => {
+  res.sendFile("index.html", { root: "client" });
+})
 
 app.post("/create_preference", async (req, res) => {
   try {
